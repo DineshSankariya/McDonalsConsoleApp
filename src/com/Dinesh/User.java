f2cf5d1343db1d661ee9da9af022a73199111a98
dinesh sankariya <dinesh.dina47@gmail.com>
2020-12-04 15:25:30 +0530

    commit on 4th dec 2020 15:25

--- src/com/Dinesh/User.java
+++ src/com/Dinesh/User.java
@@ -1,24 +1,30 @@
 package com.Dinesh;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class User<T extends Items,Combos> {
 
     private String name;
 
-    private List<T> cart;
+    private HashMap<Integer,T> cart;
 
     public User(String name) {
         this.name = name;
-        this.cart=new ArrayList<>();
+        this.cart=new HashMap<>();
+    }
+
+    public boolean add_item(T item,int quantity){
+        this.cart.put(quantity,item);
+        return true;
     }
 
     public String getName() {
         return name;
     }
 
-    public List<T> getCart() {
+    public HashMap<Integer, T> getCart() {
         return cart;
     }
 }
